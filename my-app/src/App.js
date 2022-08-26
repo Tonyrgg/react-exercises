10eaca1d9afde61fdddb575a529f2022da3b7197
Antonio <ruggieroantonio1703@gmail.com>
2022-08-26 10:08:27 +0200

    props 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ export class App extends React.Component {
     render() {
         return (
             <div>
-                <Welcome name="Tony" />
+                <Welcome />
             </div>
         )
     }
