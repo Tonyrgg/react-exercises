6cefea898a8f4515a82a5a3527e50255b109cce3
Antonio <ruggieroantonio1703@gmail.com>
2022-08-26 10:00:40 +0200

    props 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { HelloWorld } from "./HelloWorld";
+import { Welcome } from "./welcome";
 
 export class App extends React.Component {
     render() {
         return (
             <div>
-                <HelloWorld />
+                <Welcome name="Tony" />
             </div>
         )
     }
--- my-app/src/HelloWorld.js
+++ my-app/src/HelloWorld.js
@@ -3,8 +3,8 @@ import React from 'react'
 export class HelloWorld extends React.Component {
     render() {
         return <div>
-            <h1>Hello, World!!</h1> 
-            <Message />
+            {this.props.renderGreeting && <h1>Hello, World!!</h1>} 
+            <p>What a beautiful day!</p>
         </div> 
     }
 }
