e5cf551b28d69d3460835ee74f15a7fa3363ad83
Antonio <ruggieroantonio1703@gmail.com>
2022-08-26 10:44:15 +0200

    props 05

--- my-app/src/welcome.js
+++ my-app/src/welcome.js
@@ -5,8 +5,14 @@ export class Welcome extends React.Component {
         return (
             <div>
                 <p>Hello, {this.props.name}</p>
-                <p>Your age is {this.props.age}</p>
+                <Age age={this.props.age}/>
             </div>
         )
+    }
+}
+
+export class Age extends React.Component {
+    render() {
+        return <p>Your age is {this.props.age}</p>
     }
 }
