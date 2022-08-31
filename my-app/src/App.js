c24562baa554ca30eacf7327cd15c75cc43262e7
Antonio <ruggieroantonio1703@gmail.com>
2022-08-31 11:25:20 +0200

    not an exercise

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Clock } from "./Clock"
+import { Counter } from "./Counter";
 import { Welcome } from "./welcome";
 
 export class App extends React.Component {
     render() {
         return (
             <div>
-                <Welcome name="John" age="5"/>
+                <Welcome name="John" age="25"/>
+                <Clock />
+                <Counter />
             </div>
         )
     }
---
+++ my-app/src/Counter.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export class Counter extends React.Component {
+    state = {
+        count: this.props.initialValue
+    }
+    
+    constructor(props) {
+        super(props)
+
+        this._interval = setInterval(() => {
+            this.setState((state) => {
+                return {
+                    count: state.count + (this.props.incrementBy)
+                }
+            })
+        },this.props.timeout)
+    }
+
+    componentWillUnmount() {
+        if (this._interval) {
+            clearInterval(this._interval)
+        }
+    }
+
+    render() {
+        return <div>
+            <h3>Count: {this.state.count}</h3>
+            {this.state.count < 6 && <p>This counter is great!</p>}
+        </div>
+    }
+}
+
+Counter.defaultProps = {
+    timeout: 1000,
+    incrementBy: 1,
+    initialValue: 0,
+}
