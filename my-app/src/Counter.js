9dc4b4f52c3be61e330fe579268fc5446b8da7cd
Antonio <ruggieroantonio1703@gmail.com>
2022-09-06 10:15:49 +0200

    events-01

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -3,25 +3,29 @@ import { CounterDisplay } from "./CounterDisplay";
 
 export class Counter extends React.Component {
     state = {
-        count: this.props.start
+        count: this.props.initialValue
     }
 
-        componentDidMount() {
-            setInterval(() => {
-                this.setState((state) => {
-                    return {
-                        count: state.count + this.props.increment
-                    }
-                })
-            }, this.props.interval)
-        }
+    Increment = () => {
+        this.setState(state => {
+            return {
+                count: state.count + this.props.incrementBy
+            }
+        })
+    }
 
     render() {
         return (
             <div>
-                <CounterDisplay count={this.state.count}/>
+                <h3>Count: {this.state.count}</h3>
+                <button onClick={this.Increment}>Increment</button>
             </div>
             )
     }
+}
+
+Counter.defaultProps = {
+    initialValue: 0,
+    incrementBy: 1,
 }
 
