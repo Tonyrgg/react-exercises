91c1c2fdb11166c4f0fe825c48a7f75119082b60
Antonio <ruggieroantonio1703@gmail.com>
2022-09-09 18:00:30 +0200

    colors2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import { Login, UncontrolledLogin } from "./Login";
 import { Colors } from "./Mylist";
 
+const supercolori = [
+    {name: "red", id: 1, color:"rosso"},
+    {name: "green", id: 2, color: "verde"},
+    {name: "yellow", id: 3, color: "giallo"},
+]
 
 export class App extends React.Component {
     render() {
         return (
             <div>
                 <UncontrolledLogin />
-                <Colors colori={["Rosso", "Verde", "Giallo"]}/>
+                <Colors colori={supercolori}/>
             </div>
         )
     }
--- my-app/src/Mylist.js
+++ my-app/src/Mylist.js
@@ -3,7 +3,7 @@ import React from "react";
 export class Colors extends React.Component {
 
     render() {
-        const colors = this.props.colori.map(color => <li>{color}</li>)
+        const colors = this.props.colori.map(color => <li key={color.id}>Nome: {color.name}, {color.color}</li>)
 
         return (
             <div>
