25cf2e965d4b670b5feb4d711e8f34dc6ad1dd08
Antonio <ruggieroantonio1703@gmail.com>
2022-09-14 18:40:17 +0200

    usestate02

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,50 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class Login extends React.Component {
-state = {
-    name: '',
-    password: '',
-    remember: false,
+export function Login() {
+    const [data, usedata] = useState({
+        username: '',
+        password: '',
+        login: false
+    })
+
+    function changeInput(event) {
+        const {name, value, type, checked} = event.target
+
+        usedata(oldDati => ({
+            ...oldDati, [name]: type === "checkbox" ? checked : value
+        }))
+    }
+
+    function sendLogin() {
+        console.log(data);
+    }
+
+    return(
+        <div>
+            <input onChange={changeInput} name="username" value={data.username}/>
+            <input onChange={changeInput} name="password" value={data.password}/>
+            <input onChange={changeInput} type="checkbox" checked={data.remember} name="login" />
+            {data.username && data.password ? <button onClick={sendLogin}>Login</button> : <button disabled>Login</button>} 
+        </div>
+    )
 }
 
-    changeinput = (event) =>  {
-        const valore = event.target.value
-        const name = event.target.name
-        const type = event.target.type
-        const checked = event.target.checked
+// export class Login extends React.Component {
+// state = {
+//     name: '',
+//     password: '',
+//     remember: false,
+// }
+
+//     changeinput = (event) =>  {
+//         const valore = event.target.value
+//         const name = event.target.name
+//         const type = event.target.type
+//         const checked = event.target.checked
         
-        this.setState({
-            [name]: type === "checkbox" ? checked : valore,
+//         this.setState({
+//             [name]: type === "checkbox" ? checked : valore,
 
-        })
-    }
+//         })
+//     }
 
-    onLogin = () => {
-    console.log(this.state.name, this.state.password, this.state.remember);
-    }
+//     onLogin = () => {
+//     console.log(this.state.name, this.state.password, this.state.remember);
+//     }
 
-    render() {
-        return (
-                <div>
-                    <input
-                        name="name"
-                        value={this.state.name}
-                        onChange={this.changeinput}
-                    />
-                    <input 
-                        name="password"
-                        type="password"
-                        value={this.state.password}
-                        onChange={this.changeinput}
-                    />
-                    <input 
-                        name="remember"
-                        type="checkbox"
-                        checked={this.state.remember}
-                        onChange={this.changeinput}
-                    />
-                    {this.state.name && this.state.password ? <button onClick={this.onLogin}>Login</button> : <button disabled>Login</button>} 
-                </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//                 <div>
+//                     <input
+//                         name="name"
+//                         value={this.state.name}
+//                         onChange={this.changeinput}
+//                     />
+//                     <input 
+//                         name="password"
+//                         type="password"
+//                         value={this.state.password}
+//                         onChange={this.changeinput}
+//                     />
+//                     <input 
+//                         name="remember"
+//                         type="checkbox"
+//                         checked={this.state.remember}
+//                         onChange={this.changeinput}
+//                     />
+//                     {this.state.name && this.state.password ? <button onClick={this.onLogin}>Login</button> : <button disabled>Login</button>} 
+//                 </div>
+//         )
+//     }
+// }
