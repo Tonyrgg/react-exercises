a1174f5be375ba75a49863dcf5c98c9c1c38c1dd
Antonio <ruggieroantonio1703@gmail.com>
2022-09-14 19:07:32 +0200

    usestate03

--- my-app/src/Mylist.js
+++ my-app/src/Mylist.js
@@ -1,43 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-// export class Colors extends React.Component {
+export function Todolist() {
+
+    const [list, setList] = useState([])
+
+    function addString(event) {
+        event.preventDefault()  
+
+        const stringa = event.target.elements.string.value
+
+        setList(data => ([
+            ...data, stringa
+        ]))
+
+        event.target.elements.string.value = ''
+    }
+
+    return (
+        <div>
+            <form onSubmit={addString}>
+            <input name="string" />
+            <button type="submit">Add</button>
+            </form>
+            <ul>{list.map((testo, index) => <li key={testo + index}>{testo}</li>)}</ul>
+        </div>
+    )
+}
+
+// export class Todolist extends React.Component {
+//     state = {
+//         items: [],
+//     }
+
+//     addString = (event) => {
+//         event.preventDefault()
+
+//         const stringa = event.target.elements.string.value
+
+//         this.setState({
+//             items: [...this.state.items, stringa],
+//         })
+
+//     }
 
 //     render() {
-//         const colors = this.props.colori.map(color => <li key={color.id}>Nome: {color.name}, {color.color}</li>)
-
-//         return (
+//         return(
 //             <div>
-//                 <ul>{colors}</ul>
+//                 <form onSubmit={this.addString}>
+//                 <input name="string" />
+//                 <button type="submit">Add</button>
+//                 </form>
+//                 <ul>{this.state.items.map(testo => <li>{testo}</li>)}</ul>
 //             </div>
 //         )
 //     }
 // }
-
-export class Todolist extends React.Component {
-    state = {
-        items: [],
-    }
-
-    addString = (event) => {
-        event.preventDefault()
-
-        const stringa = event.target.elements.string.value
-
-        this.setState({
-            items: [...this.state.items, stringa],
-        })
-
-    }
-
-    render() {
-        return(
-            <div>
-                <form onSubmit={this.addString}>
-                <input name="string" />
-                <button type="submit">Add</button>
-                </form>
-                <ul>{this.state.items.map(testo => <li>{testo}</li>)}</ul>
-            </div>
-        )
-    }
-}
