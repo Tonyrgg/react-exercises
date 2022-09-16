d3ec5c53b5d11f7f1566a6a340536bc92fb56950
Antonio <ruggieroantonio1703@gmail.com>
2022-09-16 09:47:07 +0200

    fix

--- my-app/src/Sum.js
+++ my-app/src/Sum.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 export function Sum({ numbers = [1, 2, 3, 4]}) {
+    
+    let numero=0
+
+    numbers.map((prev)=> numero += prev)
+
     return (
-        <h1>{(numbers.reduce((prev, next)=> prev + next))}</h1>
+        <h1>{numero}</h1>
     )
 }
