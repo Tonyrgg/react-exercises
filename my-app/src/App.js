38d7774cb8b89fffd46d3a02aba79ad3bb0c08a0
Antonio <ruggieroantonio1703@gmail.com>
2022-09-21 11:05:43 +0200

    first route 1 prop

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,13 @@
 import React from "react";
-import { Container } from "./Container";
-import { DisplayLanguage } from "./DisplayLanguage";
-import { LanguageContext } from "./LanguageContext";
+import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./welcome";
 
 
 
-export class App extends React.Component {
-
-    state = {
-        language: "en"
-    }
-
-    cambioLingua = (event) => {
-        this.setState({
-            language: event.target.value
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <Welcome name="Piero"/>
-            </div>
-        )
-    }
+export function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Welcome name={"Paolo"}/>} />
+        </Routes>
+    )
 }
--- my-app/src/welcome.js
+++ my-app/src/welcome.js
@@ -1,32 +1,5 @@
 import React from "react";
 
-// export class Welcome extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 {this.props.name == "John"
-//                     ? <p>Hello, <b>{this.props.name}</b></p>
-//                     : false
-//                 }
-//                 <Age age={this.props.age}/>
-//             </div>
-//         )
-//     }
-// }
-
-// export class Age extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 {this.props.age >= 18 
-//                     ? <p>Your age is {this.props.age}</p>
-//                     : <p>You are very young!</p>
-//                 }
-//             </div>
-//         )
-//     }
-// }
-
 export function Welcome(props) {
     return(
         <div>
